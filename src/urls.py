2bc59e8bff59a04cc522999a4f634b23ad1f947d
Rafael Moulin <rafaelmoulin@gmail.com>
2012-03-14 02:01:54 -0300

    All your base are belong to us

---
+++ src/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import patterns, include, url
+from core.views import homepage
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+	(r'^$', homepage),
+    # Examples:
+    # url(r'^$', 'src.views.home', name='home'),
+    # url(r'^src/', include('src.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
