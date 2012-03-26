ec3d735d8ec5f27e156ad7da47a64458bef7b546
Rafael Moulin <rafaelmoulin@gmail.com>
2012-03-26 00:43:06 -0300

    Entrega do segundo sprint

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,5 +1,20 @@
+import datetime
 from django.contrib	import admin
 from subscriptions.models import Subscription
 
-admin.site.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'created_at', 'subscribed_today')
+    
+    list_filter = ['created_at']
+    
+    def subscribed_today(self, obj):
+        return obj.created_at.date() == datetime.date.today()
+    subscribed_today.short_description = u'Inscrito hoje?'
+    subscribed_today.boolean = True
+    
+    
+    date_hierarchy = 'created_at'
+    search_fields = ('name', 'cpf', 'email', 'phone', 'created_at')
+
+admin.site.register(Subscription, SubscriptionAdmin)
 
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11, unique=True)
+    email = models.EmailField('E-mail', unique=True)
+    phone = models.CharField('Telefone', max_length=20, blank=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
     #Essa funcao nomeia o objeto como o registro que queremos.
     #Antes quanto colocavamos Subscription.objects.all()
